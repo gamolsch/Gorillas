f778383323879eccf14c3849c08671c49229471f
= <pair+OsagieDev.christophwong@devbootcamp.com>
2014-05-02 15:47:42 -0500

    add wireframes to README, begin new_survey view

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -2,3 +2,16 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+
+### SURVEY CREATION ###
+
+  get '/surveys/new' do
+
+    erb :'survey_creation/new_survey'
+  end
+
+  post '/surveys/new' do
+
+    redirect to '/dashboard'
+  end
