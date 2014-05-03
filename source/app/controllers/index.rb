f8f281b9a404fe8bdaa82d0a75b563a0a473b1e5
Christoph Wong & <pair+christophwong.heaphyg@devbootcamp.com>
2014-05-03 17:14:54 -0500

    Front Page

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -2,3 +2,14 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/' do
+
+puts params
+redirect to "/user"
+end
+
+get "/user" do
+
+  erb :dashbored
+end
--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $("#create").on("click", function(){
+    $("#create-form").show()
+    $(".container").hide()
+    $("#create-form").submit(function(event){
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+      $.ajax(
+      {
+        url: '/',
+        type: "post",
+        data: $(this).serialize(),
+        dataType: "html",
+        success:function(data)
+        {
+          console.log("it Works!")
+        }
+      })
+
+    })
+  })
 });
