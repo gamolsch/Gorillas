dd61ec56ac58fe42720f423d530f0b3da2764341
Tom Nullet & Steve Yim <pair+nullet.stevejyim@devbootcamp.com>
2014-05-03 16:44:59 -0500

    Begin user/survey associations & bcrypt

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
   include BCrypt
+  #survey creator
+  # Remember to create a migration!
+  has_many :created_surveys, class_name: "Survey",
+            foreign_key: :creator_id
+
+  has_many :completed_surveys
+  has_many :taken_surveys, :through => :completed_surveys, :source => :surveys
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
--- source/config/environment.rb
+++ source/config/environment.rb
@@ -10,7 +10,7 @@ require 'rubygems'
 
 require 'uri'
 require 'pathname'
-
+require 'bcrypt'
 require 'pg'
 require 'active_record'
 require 'logger'
