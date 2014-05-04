81fe02c4796cb31c1b03d6f1032ed6fcae1f6c7d
Tom Nullet & Steve Yim <pair+nullet.stevejyim@devbootcamp.com>
2014-05-04 03:17:49 -0500

    follow Kevin's Ajax & CRUD video

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,9 +1,58 @@
 get '/' do
-  # Look in app/views/index.erb
+  @surveys = Survey.order('title').limit(10)
   erb :index
 end
 
+# get '/surveys' do
+#   @surveys = Survey.all
+#   erb :index
+# end
 
+# SURVEYS ======================================
+# create
+get '/surveys/new' do
+  puts session[:id]
+  erb :new_survey
+end
+
+get '/surveys/:id' do  # don't move, wildcard alert
+  puts params.inspect
+  params[:id] === 'new'
+end
+
+# create
+post '/surveys' do
+  @survey = Survey.new(params[:survey])
+  if @survey.save
+    redirect to ("/surveys/#{@survey.id}")
+  else
+    erb :new_survey
+  end
+end
+
+get '/surveys/:id/edit' do
+  @survey = Survey.find(params[:id])
+  erb :edit_survey
+end
+
+#edit
+post '/surveys/:id' do
+  @survey = Survey.find(params[:id])
+  if @survey.update_attributes(params[:survey])
+    redirect to("/surveys/#{@survey.id}")
+  else
+    erb :edit_survey
+  end
+end
+
+post '/surveys/:id/delete' do
+  @survey = Survey.find(params[:id])
+  @survey.destroy
+  redirect to ("/surveys")
+end
+
+
+# USERS ======================================
 post '/' do
   @user = User.create(email: params[:email], first_name: params[:firstname], last_name: params[:lastname])
 #  @user.password = params[:password]
@@ -13,7 +62,6 @@ post '/' do
 end
 
 get "/user" do
-
   erb :dashbored
 end
 
@@ -22,35 +70,13 @@ post '/login' do
   @user = User.find_by_email(params[:email])
   if @user.password == params[:password]
     session[:id] = @user.id
-
     redirect to "/user"
   else
    redirect to '/'
   end
 end
 
-### SURVEY CREATION ###
 
-get '/surveys/new' do
-  puts session[:id]
-  erb :'survey_creation/new_survey'
-end
 
-post '/surveys/new' do
-  @survey = Survey.create(title: params[:title])
 
-  @question = Question.create(survey_id: @survey.id,
-                              question: params[:question])
-
-  @choices = Choice.create(question_id: @question.id,
-                            choice: params[:choice_1])
-  @choices = Choice.create(question_id: @question.id,
-                            choice: params[:choice_2])
-  @choices = Choice.create(question_id: @question.id,
-                            choice: params[:choice_3])
-  @choices = Choice.create(question_id: @question.id,
-                            choice: params[:choice_4])
-
-  redirect to '/surveys/new'
-end
 
--- source/db/migrate/20140502111850_create_questions.rb
+++ source/db/migrate/20140502111850_create_questions.rb
@@ -6,30 +6,35 @@ class CreateQuestions < ActiveRecord::Migration
 
   		t.timestamps
   	end
-        create_table :surveys do |t|
+
+      create_table :surveys do |t|
       t.string :title
 
       t.belongs_to :creator
       t.timestamps
     end
+
       create_table :users do |t|
       t.string :email, :first_name, :last_name, :password_hash
 
       t.timestamps
     end
-        create_table :choices do |t|
+
+      create_table :choices do |t|
       t.belongs_to :question
       t.string :choice
 
       t.timestamps
     end
-        create_table :user_choices do |t|
+
+      create_table :user_choices do |t|
       t.belongs_to :user
       t.belongs_to :choice
 
       t.timestamps
     end
-        create_table :completed_surveys do |t|
+
+      create_table :completed_surveys do |t|
       t.belongs_to :survey
       t.belongs_to :user
       t.integer :survey_taker_id
